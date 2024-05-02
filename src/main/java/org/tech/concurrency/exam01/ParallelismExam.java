182cb47f0d3eac1813aa1cdc96af12dfd9f84077
kwongyumin <rbals7398@naver.com>
2024-05-02 17:21:56 +0900

    - 동시성 및 병렬성 동작 과정 예시코드 구현

---
+++ src/main/java/org/tech/concurrency/exam01/ParallelismExam.java
@@ -0,0 +1,40 @@
+package org.tech.concurrency.exam01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelismExam {
+
+    public static void main(String[] args) {
+//        int cpuCores = 1;
+        int cpuCores = Runtime.getRuntime().availableProcessors();
+
+        System.out.println(cpuCores);
+
+        // CPU 개수만큼 데이터를 생성
+        List<Integer> data = new ArrayList<>();
+        for (int i = 0; i < cpuCores; i++) {
+            data.add(i);
+        }
+
+        // CPU 개수만큼 데이터를 병렬로 처리
+        long startTime1 = System.currentTimeMillis();
+        long sum1 = data.parallelStream()
+                .mapToLong(i -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return i * i;
+                })
+                .sum();
+
+        long endTime1 = System.currentTimeMillis();
+
+        System.out.println("CPU 개수만큼 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime1 - startTime1) + "ms");
+        System.out.println("결과1: " + sum1);
+
+    }
+
+}
