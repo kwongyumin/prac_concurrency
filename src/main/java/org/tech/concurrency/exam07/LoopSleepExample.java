49c4e807559d71fa5bebde68a2843739d6beff43
kwongyumin <rbals7398@naver.com>
2024-06-13 23:10:04 +0900

    - Java Thread Fundamentals (5)

---
+++ src/main/java/org/tech/concurrency/exam07/LoopSleepExample.java
@@ -0,0 +1,17 @@
+package org.tech.concurrency.exam07;
+
+public class LoopSleepExample {
+    public static void main(String[] args) {
+
+        for(int i =0; i<7; i++) {
+            try {
+                System.out.println("반복: " + (i + 1) );
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
